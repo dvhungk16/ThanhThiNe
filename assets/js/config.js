aecc0bb47223c73fbf8ca083ad1ba17ae02c3b47
Hung Do <hungyasuo2510@gmail.com>
2024-04-25 12:10:20 +0700

    add file

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+
+const nameGirl = '';
+const giftUrl = 'https://www.instagram.com/stories/highlights/17882443061933720/';
+const eventName = 'Tặng cậu cái này không vì ngày gì cả !!';
+const titleCard = 'Tặng cô ấy';
+const contentCard = 'Chúc cô ấyy luôn xinh đẹp, học giỏi và mãi mãi là người vô gia cuuu nhé ^^^';
+
+const giftImage = '/assets/img/Screenshot 2024-04-25 011918.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
